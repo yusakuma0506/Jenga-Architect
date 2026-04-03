581587a79c970e972b5659c05abb8c12560080ed
yusakuma0506 <yu.sakuma0506@gmail.com>
2026-04-03 18:54:11 +0800

    fix ci cd

--- src/app/api/auth/signin/page.tsx
+++ src/app/api/auth/signin/page.tsx
@@ -9,7 +9,7 @@ export default function SignInPage() {
       <div className="p-8 z-[1000] bg-gradient-to-r from-indigo-600 to-violet-600 rounded-xl shadow-lg flex flex-col gap-4">
         <h2 className="text-xl font-bold mb-4 text-center">Login to Jenga Architect</h2>
         <button 
-          onClick={() => signIn("google", { callbackUrl: "/" })}
+          onClick={() => signIn("google", {callbackUrl: "/" })}
           className="bg-white text-black p-3 rounded font-bold w-64 flex items-center hover:bg-gray-300 active:scale-95"
         >
             <Image src="/google.png" alt="google icon" width={24} height ={24} className="mx-2 mr-4"/>
@@ -17,7 +17,7 @@ export default function SignInPage() {
           
         </button>
         <button 
-          onClick={() => signIn("github", { callbackUrl: "/" })}
+          onClick={() => signIn("github", {callbackUrl: "/" })}
           className="bg-gray-500 text-white p-3 rounded font-bold w-64 flex items-center hover:bg-gray-600 active:scale-95"
         >
             <Image src="/github.png" alt="github icon" width={24} height ={24} className="mx-2 mr-4"/>
